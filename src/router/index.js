8fbf5ca48ed4f89ce0d6f56a568b950220c6fe4b
shijun <jun.shi5@pactera.com>
2018-02-22 15:18:31 +0800

    update

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import Axios from 'axios'
+import Login from '@/components/Login'
+import Registrate from '@/components/Registrate'
+import Success from '@/components/Success'
+Vue.use(Router)
+Vue.use(ElementUI)
+Vue.prototype.$ajax = Axios
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/Registrate',
+      name: 'Registrate',
+      component: Registrate
+    },
+    {
+      path: '/success',
+      name: 'Success',
+      component: Success
+    }
+  ]
+})
